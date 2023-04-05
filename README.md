b3efd97e8bb97be14efe1fc091060cdfeec91dfb
Fernando Serrano Carpena <ferserc1@gmail.com>
2023-04-05 13:07:51 +0200

    Video360CanvasPlugin implemented

--- README.md
+++ README.md
@@ -1,147 +1,45 @@
-# paella-zoom-plugin
+# paella-webgl-plugins
 
-A plugin to zoom videos for Paella Player.
-
-This set of plugins consists of four different plugins:
-
-- zoom plugin: is the main plugin, of canvas type, and provides the ability to zoom in and out on video streams. It's mandatory to activate this plugin for the rest of the plugins in this set to work
-
-**data.json:**
-
-```mixed
-{
-  ...
-  "streams": [
-    {
-      "sources": {
-        ...
-      },
-      "content": "presenter",
-      "canvas": ["video"]  << "video" is the default canvas
-    }
-  ]
-}
-```
-
-Note that, as "video" is the default canvas type, it's not mandatory that the stream is configured with the `canvas` attribute to use the zoom canvas.
-
-- zoom menu button plugin: adds a button that displays a menu with the options to zoom in and out.
-- Zoom in button plugin: adds a button that enlarges the video
-- Zoom out button plugin: adds a button that reduces the video.
+A set of canvas plugins to show videos using a WebGL canvas.
 
 
 
-## Zoom plugin
+## Video 360 Canvas
 
-This is a canvas plugin, which will work on videos whose canvas is set to "video" in the video manifest. Note that for the zoom to work, you must disable the default canvas in the configuration file, or set a higher priority:
+It is a canvas plugin that allows you to display 360-degree videos recorded in equirectangular format. Mouse or touch actions on the canvas are used to zoom and scroll the video, therefore with this canvas the play/pause by clicking on the video canvas is disabled.
 
-```mixed
-{
-  "plugins": {
-     ...
-    
-     "es.upv.paella.videoCanvas": {
-      	"enabled": false,		< deactivate default canvas	
-      	"order": 1		< or set lower priority
-      },
-
-      "es.upv.paella.zoomPlugin": {
-    		"enabled": true,
-        "order": 0,
-        "maxZoom": 800,
-        "showButtons": true
-      },
-			...
-	}
-}
-```
-
-Zooming works with the mouse scroll wheel by holding down the `alt` key. It is also possible to show zoom in and zoom out buttons on the video canvas by activating the `showButtons` option.
-
-![zoom_buttons.jpg](zoom_buttons.jpg)
-
-**Exported as** `ZoomCanvasPlugin`.
-
-##  Zoom menu plugin
-
-Displays the options to zoom in or zoom out the video using a drop-down menu. This plugin works on a specific target, which matches the `content` attribute of the video manifest:
+This video canvas works with those streams that have `video360` as canvas identifier in the [video manifest](https://paellaplayer.upv.es/#/doc/video_manifest.md).
 
 **data.json:**
 
-```mixed
+```json
 {
-  ...
-  "streams": [
-    {
-      "sources": {
-        ...
-      },
-      "content": "presenter",	<< content target to zoom
-      "canvas": ["video"]
-    }
-  ]
+    ...
+    "streams": [
+        {
+            "sources": {...},
+            "content": "presenter",
+            "canvas":["video360"]
+        }
+    ]
 }
 ```
-
 
 **config.json:**
 
 ```json
 {
-  "plugins": {
-    "es.upv.paella.zoomMenuButtonPlugin": {
-      "enabled": true,
-        "parentContainer": "playbackBar",
-        "side": right,
-        "target": "presenter"		<< content target to zoom
-      }
-	}
+    "plugins": {
+        "es.upv.paella.video360Canvas": {
+            "enabled": true,
+            "maxZoom": 2,
+            "minZoom": 0.5,
+            "speedX": 0.4,
+            "speedY": 0.4
+        }
+    }
 }
 ```
 
-**Exported as** `ZoomMenuButtonPlugin`.
-
-**Icon customization data:**
-
-- Plugin identifier: `es.upv.paella.zoomMenuButtonPlugin`
-- Icon names:
-    * `zoomInIcon`
-    * `zoomOutIcon`
-
-
-## Zoom in and zoom out buttons
-
-These are two button plugins that allow you to zoom in and zoom out the video independently, instead of being grouped in a menu as with the `zoomMenuPlugin` plugin.
-
-**config.json:**
-
-```json
-{
-  "plugins": {
-      "es.upv.paella.zoomInButtonPlugin": {
-    		"enabled": true,
-        "parentContainer": "videoContainer",
-        "side": right,
-        "target": "presenter"
-      },
-	    "es.upv.paella.zoomOutButtonPlugin": {
-    		"enabled": true,
-        "parentContainer": "videoContainer",
-        "side": right,
-        "target": "presenter"
-      }
-	}
-}
-```
-
-**Exported as** `ZoomInButtonPlugin` and `ZoomOutButtonPlugin`.
-
-**Icon customization data:**
-
-- Plugin identifier: `es.upv.paella.zoomInButtonPlugin`
-- Icon names:
-    * `zoomInIcon`
-
-- Plugin identifier: `es.upv.paella.zoomOutButtonPlugin`
-- Icon names:
-    * `zoomOutIcon`
+**Exported as** `Video360CanvasPlugin`.
+¡
--- src/plugins/es.upv.paella.video360Canvas.js
+++ src/plugins/es.upv.paella.video360Canvas.js
@@ -7,7 +7,8 @@ import { createSphere } from '../js/primitives';
 import Mat4 from '../js/math/Mat4';
 
 const vertex = `
-attribute vec4 inVertexPos;
+precision highp float;
+attribute vec3 inVertexPos;
 attribute vec2 inTexCoord;
 
 varying vec2 fsTexCoord;
@@ -16,7 +17,7 @@ uniform mat4 uView;
 uniform mat4 uProj;
 
 void main() {
-    gl_Position = uProj * uView * inVertexPos;
+    gl_Position = uProj * uView * vec4(inVertexPos,1.0);
     fsTexCoord = inTexCoord;
 }
 `;
@@ -29,7 +30,6 @@ varying vec2 fsTexCoord;
 
 void main() {
     gl_FragColor = texture2D(uVideoTexture,fsTexCoord);
-    // gl_FragColor = vec4(fsTexCoord, 0.0, 1.0);
 }
 `;
 
@@ -51,6 +51,10 @@ export class Video360Canvas extends Canvas {
     constructor(player, videoContainer, config) {
         super('canvas', player, videoContainer);
         this.config = config;
+        this.maxZoom = config.maxZoom ?? 3;
+        this.minZoom = config.minZoom ?? 0.3;
+        this.speedX = config.speedX ?? 1;
+        this.speedY = config.speedY ?? 1;
 
         let downPosition = [0,0];
         let drag = false;
@@ -71,19 +75,40 @@ export class Video360Canvas extends Canvas {
             evt.stopPropagation();
         });
 
+        this.element.addEventListener('wheel', evt => {
+            this.currentZoom += evt.deltaY / 1000;
+            if (this.currentZoom > this.maxZoom) {
+                this.currentZoom = this.maxZoom;
+            }
+            else if (this.currentZoom < this.minZoom) {
+                this.currentZoom = this.minZoom;
+            }
+        });
+
+        this.element.addEventListener('mouseout', evt => {
+            drag = false;
+            evt.stopPropagation();
+        });
+
         this.element.addEventListener('mousemove', evt => {
             if (drag) {
                 const newPos = [evt.clientX, evt.clientY];
                 const diff = [downPosition[0] - newPos[0], downPosition[1] - newPos[1]];
                 downPosition = newPos;
-                console.log(diff);
-                this._yaw += diff[0] * 0.008;
-                this._pitch += diff[1] * 0.004;
+                this._yaw += diff[0] * 0.004 * this.speedX;
+                this._pitch += diff[1] * 0.004 * this.speedY;
                 if (this._yaw>2*Math.PI) {
                     this._yaw = this._yaw - 2 * Math.PI;
                 }
                 else if (this._yaw<0) {
                     this._yaw = 2 * Math.PI + this._yaw;
+                }
+
+                if (this._pitch>Math.PI / 2) {
+                    this._pitch = Math.PI / 2;
+                }
+                else if (this._pitch<=-Math.PI / 2) {
+                    this._pitch = -Math.PI / 2;
                 }
 
             }
@@ -106,7 +131,7 @@ export class Video360Canvas extends Canvas {
 
         this._videoPlayer = player;
         player.element.crossOrigin = "";
-        removeVideoElement.apply(this, [player.element, true])
+        removeVideoElement.apply(this, [player.element, false])
 
         this._webGLCanvas = new WebGLCanvas(this.element);
         await this._webGLCanvas.init({ clearColor: [0.2, 0.3, 0.8, 1] });
@@ -121,39 +146,17 @@ export class Video360Canvas extends Canvas {
             uniforms: [ 'uView', 'uProj', 'uVideoTexture' ]
         });
 
-        const sphereData = createSphere(10);
-        console.log(sphereData);
-
-        const sphere = new SceneObject(gl, sphereData.positions.flat(), sphereData.uvs.flat(), sphereData.triangles.flat());
-
-        const quad = new SceneObject(gl, 
-            [
-                 1, 1, 0,
-                -1, 1, 0,
-                -1,-1, 0,
-                 1,-1, 0
-            ],
-            [
-                1, 0,
-                0, 0,
-                0, 1,
-                1, 1
-            ],
-            [
-                0, 1, 2,
-                2, 3, 0
-            ]);
-
-        const projection = Mat4.MakePerspective(45.0, 1, 0.01, 10.0);
-        const view = Mat4.MakeTranslation(0, 0, -5);
+        const sphereData = createSphere(30);
+        const sphere = new SceneObject(gl, sphereData.positions, sphereData.uvs, sphereData.triangles);
         
-        //const mat = Mat4.Mult(projection, translation);
-     
-        this._rot = 0;
         const canvas = this.element;
         const draw = () => {
-            this._rot = this._yaw;
-            const model = Mat4.MakeRotation(this._rot, 0, 1, 0);
+            const fov = 45 * this.currentZoom;
+            const projection = Mat4.MakePerspective(fov, 1.33, 0.01, 10.0);
+            const view = Mat4.MakeIdentity();
+            const model = Mat4.MakeRotation(this._yaw, 0, 1, 0);
+            model.rotate(this._pitch, 1, 0, 0);
+
             videoTexture.updateTexture();
             const w = canvas.clientWidth;
             const h = canvas.clientHeight;
@@ -163,14 +166,11 @@ export class Video360Canvas extends Canvas {
             gl.clear(gl.COLOR_BUFFER_BIT);
     
             shader.useProgram();
-            //shader.enablePositionArray('inVertexPos', quad);
-            //shader.enableTexCoordArray('inTexCoord', quad);
             shader.enablePositionArray('inVertexPos', sphere);
-            shader.enablePositionArray('inTexCoord', sphere);
+            shader.enableTexCoordArray('inTexCoord', sphere);
             shader.bindMatrix4('uView', Mat4.Mult(model,view));
             shader.bindMatrix4('uProj', projection);
             shader.bindTexture('uVideoTexture', videoTexture.texture);
-            //quad.draw();
             sphere.draw();
             requestAnimationFrame(draw);    
         }
